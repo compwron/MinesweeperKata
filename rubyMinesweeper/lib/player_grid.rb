30644363d1f170ce41c4374da2c87303c683f2c5
compwron <wordymusic@gmail.com>
2013-08-02 17:24:44 -0500

    WIP mapping

--- rubyMinesweeper/lib/player_grid.rb
+++ rubyMinesweeper/lib/player_grid.rb
@@ -1,10 +1,30 @@
 class PlayerGrid
 
 	def initialize grid
-		@grid = grid
+		@grid = translate_grid(grid)
 	end
 
 	def to_s
-		"* 1\n1 1"
+		@grid.map{ |line|
+			line.join(' ')
+		}.join("\n")
+	end
+
+	private
+	def translate_grid grid
+		grid.each_with_index.map {|line, line_index|
+			line.each_with_index.map {|item, item_index|
+				is_star?(item) ? '*' : calculate_nearness_to_bomb([line_index, item_index], grid)
+			}
+		}
+	end
+
+	def is_star? item
+		item
+	end
+
+	def calculate_nearness_to_bomb position, grid
+		0
+		# grid[position.first][position.last]
 	end
 end
--- rubyMinesweeper/spec/player_grid_spec.rb
+++ rubyMinesweeper/spec/player_grid_spec.rb
@@ -2,11 +2,18 @@ require_relative "../lib/player_grid"
 
 describe PlayerGrid do
 
-  subject { PlayerGrid.new [['*', '.'], ['.', '.']]} 
-
   describe "#to_s" do
     it "should translate to nearest strucutre" do
-    	subject.to_s.should == "* 1\n1 1"
+    	a = Array.new(2) {Array.new(2, false)}
+    	a[0][0] = true
+    	grid = PlayerGrid.new a
+
+    	grid.to_s.should == "* 1\n1 1"
 		end
+
+		# it "should translate to nearest structure" do
+		# 	grid = PlayerGrid.new [Array.new(3, Array.new(3, false))]
+  #   	grid.to_s.should == "* 1\n1 1\n 0 0"
+		# end
 	end
 end
