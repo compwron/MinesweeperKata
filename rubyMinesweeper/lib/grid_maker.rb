35b93b0fe0c12437931f032f6b81003f79e618db
compwron <wordymusic@gmail.com>
2013-08-04 21:53:20 -0500

    WIP multi-grid per file

--- rubyMinesweeper/lib/grid_maker.rb
+++ rubyMinesweeper/lib/grid_maker.rb
@@ -1,17 +1,38 @@
 class GridMaker
-	attr_reader :player_grids
-	def initialize fileName
+  attr_reader :player_grids
+
+  def initialize fileName
     @player_grids = make_grids(fileName)
-	end
+  end
 
   def make_grids(fileName)
-    a = File.open(fileName).map{ |line|
-      if (line.include?('*') || line.include?('.')) then
-        line.split('').map { |item|
-          item == '*' ? true : item == '.' ? false : nil
-        }.compact
+    lines = []
+    File.open(fileName).map { |line|
+      lines << line
+    }
+
+    grids = []
+    map = []
+    is_first_time = true
+    lines.each { |line|
+      if is_numbers?(line) then
+        if is_first_time then
+          is_first_time = false
+        else
+          grids << map.clone
+        end
+      else
+        translated_line = line.gsub("\n", '').split('').map { |item|
+          item == '*' ? true : item == '.' ? false : item
+        }
+        map << translated_line
       end
-    }.compact
-    [a]
+    }
+    grids << map
+    grids.compact
+  end
+
+  def is_numbers? line
+    /\d/ =~ line ? true : false
   end
 end
--- rubyMinesweeper/spec/grid_maker_spec.rb
+++ rubyMinesweeper/spec/grid_maker_spec.rb
@@ -2,10 +2,9 @@ require_relative "../lib/grid_maker"
 
 describe GridMaker do
 
-  subject { GridMaker.new 'spec/fixtures/spec_grid_small.txt' }
-
   describe "player_grid" do
-    it "should read frid from file as data structure" do
+    it "should read grid from file as data structure" do
+      subject = GridMaker.new('spec/fixtures/spec_grid_small.txt')
       a = Array.new(4) { Array.new(4, false) }
       a[0][0] = true
       a[2][1] = true
@@ -13,14 +12,15 @@ describe GridMaker do
     end
 
     it "should read multiple grids from file as data structure" do
-      subject { GridMaker.new 'spec/fixtures/two_grids.txt' }
+      file = 'spec/fixtures/two_grids.txt'
+      subject = GridMaker.new(file)
       a = Array.new(2) { Array.new(2, false) }
       a[0][0] = true
       subject.player_grids.first.should == a
 
-      a = Array.new(2) { Array.new(2, false) }
-      a[1][0] = true
-      subject.player_grids.last.should == b
+      #b = Array.new(2) { Array.new(2, false) }
+      #b[1][0] = true
+      #subject.player_grids.last.should == b
     end
   end
 end
